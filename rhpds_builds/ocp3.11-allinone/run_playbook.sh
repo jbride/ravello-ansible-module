5f33a8c266e68a7917868f4834b5299f46e7d32c
Patrick Rutledge <prutledg@redhat.com>
2019-02-19 10:18:59 -0500

    added all in one build

--- rhpds_builds/ocp3.11-allinone/run_playbook.sh
+++ rhpds_builds/ocp3.11-allinone/run_playbook.sh
@@ -9,7 +9,7 @@ fi
 if [ -z "$APP_NAME" ]
 then
   # Set this to whatever you want the app to be called
-  APP_NAME="OCP-v3.11-CNS-Baseline"
+  APP_NAME="OCP-v3.11-allinone"
   datest=`date +%Y%m%d%H%M%S`
   export APP_NAME="${APP_NAME}-${datest}"
 fi
