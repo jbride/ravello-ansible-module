5cab45f7c52a85a744cce093359d3213b3094378
Patrick Rutledge <prutledg@redhat.com>
2019-02-20 18:39:32 -0500

    got rid of all in one and replaced with minimal build

--- rhpds_builds/ocp3.11minimal.app/run_playbook.sh
+++ rhpds_builds/ocp3.11minimal.app/run_playbook.sh
@@ -9,7 +9,7 @@ fi
 if [ -z "$APP_NAME" ]
 then
   # Set this to whatever you want the app to be called
-  APP_NAME="OCP-v3.11-allinone"
+  APP_NAME="OCP-v3.11-Minimal-Baseline"
   datest=`date +%Y%m%d%H%M%S`
   export APP_NAME="${APP_NAME}-${datest}"
 fi
